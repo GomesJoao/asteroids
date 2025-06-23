13b0b033a6d7ae7c547c6f17b2f424f691ad8c4a
GomesJoao <joao.gomes.dnd@gmail.com>
2025-06-24 00:27:57 +0100

    Added turnspeed and assigned it to keys A and D, using the update() method from the Player class in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-import player
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +10,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player_character = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        
+        player.update(dt)
+
         screen.fill("black")
-        player_character.draw(screen)
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
