b846f6099e9a2da130a91d38ac4cc0bb6f0df672
GomesJoao <joao.gomes.dnd@gmail.com>
2025-06-24 22:12:05 +0100

    Added shooting cooldown, set to PLAYER_SHOOT_COOLDOWN

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -24,6 +25,7 @@ class Player(CircleShape):
         self.rotation += dt * PLAYER_TURN_SPEED
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -38,10 +40,13 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
-        newshot = Shot(self.position.x, self.position.y)
-        newshot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.timer >= 0:
+            pass
+        else:
+            newshot = Shot(self.position.x, self.position.y)
+            newshot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
-    
     def move(self, dt):
         #We start with a unit vector pointing straight up from (0, 0) to (0, 1).
         #We rotate that vector by the player's rotation, so it's pointing in the direction the player is facing.
